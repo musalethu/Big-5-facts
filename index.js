d3eef20678b11b212461f12f4cf7e0b32c303d24
Selfmade20 <ratshikombofarisani@gmail.com>
2021-04-29 13:24:19 +0200

    big five animals

---
+++ index.js
@@ -0,0 +1,36 @@
+const setNewImage =()=>{
+document.getElementById("lion").src = "https://cdn.pixabay.com/photo/2016/01/15/08/55/lion-1141303_960_720.jpg";
+
+}
+const setOldImage =() =>{
+    document.getElementById("lion").src = "";
+  
+}
+const setNewLeopard =()=>{
+    
+    document.getElementById("leopard").src =
+      "https://images.pexels.com/photos/259434/pexels-photo-259434.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+}
+const setOldLeopard =()=>{
+    document.getElementById("leopard").src = "";
+
+}
+const setNewRhino=()=>{
+    document.getElementById("rhino").src = "https://cdn.pixabay.com/photo/2013/08/11/19/47/rhino-171654_960_720.jpg";
+}
+const setOldRhino=()=>{
+    document.getElementById("rhino").src = "";
+}
+const setNewBuffalo=()=>{
+    document.getElementById("buffalo").src =
+      "https://images.pexels.com/photos/6436686/pexels-photo-6436686.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+}
+const setOldBuffalo=()=>{
+    document.getElementById("buffalo").src ="";
+}
+const setNewElephant=()=>{
+    document.getElementById("elephant").src = "https://cdn.pixabay.com/photo/2020/01/24/22/32/elephant-4791438_960_720.jpg";
+}
+const setOldElephant = () => {
+  document.getElementById("elephant").src ="";
+};
