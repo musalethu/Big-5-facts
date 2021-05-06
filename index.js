b7797c68d65087aa30412be160e7def318894d69
Selfmade20 <ratshikombofarisani@gmail.com>
2021-05-06 11:46:24 +0200

    check modified files

--- index.js
+++ index.js
@@ -1,36 +1,104 @@
-const setNewImage =()=>{
+
+
+function playAudio(soundObj) {
+    var audio = document.getElementById(soundObj);
+    audio.play(soundObj);
+}
+
+function pauseAudio() {
+    var audio = document.getElementById("soundObj");
+  audio.pause();
+}
+
+function playAudio(soundObj1) {
+  var audio = document.getElementById(soundObj1);
+  audio.play(soundObj1);
+}
+
+function pauseAudio() {
+  var audio = document.getElementById("soundObj1");
+  audio.pause();
+}
+
+function playAudio(soundObj2) {
+  var audio = document.getElementById(soundObj2);
+  audio.play(soundObj2);
+}
+
+function pauseAudio() {
+  var audio = document.getElementById("soundObj2");
+  audio.pause();
+}
+
+function playAudio(soundObj3) {
+  var audio = document.getElementById(soundObj3);
+  audio.play(soundObj3);
+}
+
+function pauseAudio() {
+  var audio = document.getElementById("soundObj3");
+  audio.pause();
+}
+
+function playAudio(soundObj4) {
+  var audio = document.getElementById(soundObj4);
+  audio.play(soundObj4);
+}
+
+function pauseAudio() {
+  var audio = document.getElementById("soundObj4");
+  audio.pause();
+}
+
+
+
+
+
+
+
+
+
+
+
+// console.log(playAudio);
+
+const setNewImage = () => {
 document.getElementById("lion").src = "https://cdn.pixabay.com/photo/2016/01/15/08/55/lion-1141303_960_720.jpg";
 
 }
 const setOldImage =() =>{
-    document.getElementById("lion").src = "";
+    document.getElementById("lion").src = "img/BROWN LION.jpg";
   
 }
 const setNewLeopard =()=>{
     
     document.getElementById("leopard").src =
-      "https://images.pexels.com/photos/259434/pexels-photo-259434.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+ "https://images.pexels.com/photos/259434/pexels-photo-259434.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
 }
 const setOldLeopard =()=>{
-    document.getElementById("leopard").src = "";
+    document.getElementById("leopard").src = "img/brownField.jpg";
 
 }
 const setNewRhino=()=>{
     document.getElementById("rhino").src = "https://cdn.pixabay.com/photo/2013/08/11/19/47/rhino-171654_960_720.jpg";
 }
 const setOldRhino=()=>{
-    document.getElementById("rhino").src = "";
+    document.getElementById("rhino").src =
+      "https://cdn.pixabay.com/photo/2013/02/27/14/50/africa-86621_960_720.jpg";
 }
 const setNewBuffalo=()=>{
     document.getElementById("buffalo").src =
       "https://images.pexels.com/photos/6436686/pexels-photo-6436686.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
 }
 const setOldBuffalo=()=>{
-    document.getElementById("buffalo").src ="";
+    document.getElementById("buffalo").src =
+      "https://images.pexels.com/photos/6052992/pexels-photo-6052992.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
 }
 const setNewElephant=()=>{
-    document.getElementById("elephant").src = "https://cdn.pixabay.com/photo/2020/01/24/22/32/elephant-4791438_960_720.jpg";
+    document.getElementById("elephant").src =
+      "https://images.pexels.com/photos/2751693/pexels-photo-2751693.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
 }
 const setOldElephant = () => {
-  document.getElementById("elephant").src ="";
+    document.getElementById("elephant").src =
+        "https://cdn.pixabay.com/photo/2017/12/21/04/54/elephant-3031112_960_720.jpg";
 };
